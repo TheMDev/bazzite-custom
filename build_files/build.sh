d9a9d88002666c478d712243be6858cb188fb0c6
Michael Wagner <19788068+TheMDev@users.noreply.github.com>
2025-11-02 23:45:12 -0600

    Update GNOME Shell Extensions

--- build_files/build.sh
+++ build_files/build.sh
@@ -9,8 +9,14 @@ mkdir -p /var/opt
 dnf5 install -y \
     android-tools \
     btrfsmaintenance \
+    cockpit \
+    cockpit-files \
+    cockpit-machines \
+    cockpit-ostree \
+    cockpit-podman \
     containernetworking-plugins \
     flatpak-builder \
+    glib2-devel \
     podman-compose \
     podman-machine \
     podman-tui \
@@ -23,7 +29,21 @@ dnf5 install -y \
 ### Install gnome extensions
 dnf5 install -y \
     gnome-shell-extension-apps-menu \
-    gnome-shell-extension-drive-menu
+    gnome-shell-extension-dash-to-dock \
+    gnome-shell-extension-dash-to-panel \
+    gnome-shell-extension-drive-menu \
+    gnome-shell-extension-launch-new-instance \
+    gnome-shell-extension-places-menu \
+    gnome-shell-extension-user-theme \
+    gnome-shell-extension-window-list
+
+### Remove Bazzite gnome extensions
+rm -rf /usr/share/gnome-shell/extensions/block-caribou-36@lxylxy123456.ercli.dev
+rm -rf /usr/share/gnome-shell/extensions/burn-my-windows@schneegans.github.com
+rm -rf /usr/share/gnome-shell/extensions/compiz-windows-effect@hermes83.github.com
+rm -rf /usr/share/gnome-shell/extensions/compiz-alike-magic-lamp-effect@hermes83.github.com
+rm -rf /usr/share/gnome-shell/extensions/desktop-cube@schneegans.github.com
+rm -rf /usr/share/gnome-shell/extensions/hotedge@jonathan.jdoda.ca
 
 ### Install from copr
 # dnf5 copr enable user/project
