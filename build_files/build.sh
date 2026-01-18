0c18e90fc1a397ae9048e53cd575eba56559ba88
Michael Wagner <19788068+TheMDev@users.noreply.github.com>
2026-01-17 19:03:12 -0600

    Test Mutter 49.3

--- build_files/build.sh
+++ build_files/build.sh
@@ -5,6 +5,14 @@ set -ouex pipefail
 # /opt directory fix
 mkdir -p /var/opt
 
+# Temporary Mutter Xwayland Fractional Scaling Cursor Constraint Patch
+# Remove When Mutter 49.3 & 50.0 Are Up Stream.
+# Remove Bazzite's Mutter Version Lock
+dnf5 versionlock delete mutter
+dnf5 copr enable -y mdwagner/mutter
+dnf5 copr enable -y ublue-os/bazzite-multilib
+dnf5 swap -y --repo=copr:copr.fedorainfracloud.org:mdwagner:mutter mutter mutter-49.3-1.fc43.x86_64
+
 ### Install packages
 dnf5 install -y \
     android-tools \
@@ -15,6 +23,7 @@ dnf5 install -y \
     cockpit-ostree \
     cockpit-podman \
     containernetworking-plugins \
+    fedpkg \
     flatpak-builder \
     glib2-devel \
     nodejs \
@@ -51,7 +60,6 @@ rm -rf /usr/share/gnome-shell/extensions/hotedge@jonathan.jdoda.ca
 ### Install from copr
 # dnf5 copr enable user/project
 # dnf5 install -y \
-#     r2modman
 
 ### Install Kopia
 dnf5 config-manager addrepo --from-repofile=/ctx/repo_files/kopia.repo
